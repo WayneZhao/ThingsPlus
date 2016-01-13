dd32acfaa98949c28e1940eaa5844103edabc3f6
WayneZhao <wayne_3475@163.com>
2016-01-13 19:02:49 +0800

    Update Framework

---
+++ app/src/main/java/com/dremer/framework/thingsplus/app/ThingsPlusApplication.java
@@ -0,0 +1,9 @@
+package com.dremer.framework.thingsplus.app;
+
+import android.app.Application;
+
+/**
+ * Created by zx on 2016/1/13.
+ */
+public class ThingsPlusApplication extends Application {
+}
