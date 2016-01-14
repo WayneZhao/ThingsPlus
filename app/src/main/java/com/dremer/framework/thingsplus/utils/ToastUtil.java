ae42bcc0641a1d3e5c7b880cfe43a90b3e0ae89b
WayneZhao <wayne_3475@163.com>
2016-01-14 17:35:40 +0800

    Update Framework

---
+++ app/src/main/java/com/dremer/framework/thingsplus/utils/ToastUtil.java
@@ -0,0 +1,30 @@
+package com.dremer.framework.thingsplus.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by zx on 2016/1/14.
+ */
+public class ToastUtil {
+    private ToastUtil() {
+    }
+
+    public static void showToast(Context context, String toast) {
+        if (null == mToast) {
+            mToast = Toast.makeText(context, toast, Toast.LENGTH_LONG);
+        } else {
+            mToast.setText(toast);
+        }
+
+        mToast.show();
+    }
+
+    public static void cancel() {
+        if (null != mToast) {
+            mToast.cancel();
+        }
+    }
+
+    public static Toast mToast = null;
+}
